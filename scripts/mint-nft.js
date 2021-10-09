db3e33973d30d638907cd71badd69c4aa238f728
jaolan <olanjorel@gmail.com>
2021-10-09 12:39:20 -0500

    Add generate contract from ABI, add NFT metadata

--- scripts/mint-nft.js
+++ scripts/mint-nft.js
@@ -3,3 +3,10 @@ const API_URL = process.env.API_URL
 const { createAlchemyWeb3 } = require("@alch/alchemy-web3")
 const web3 = createAlchemyWeb3(API_URL)
 
+// parse contents of contract ABI
+const contract = require("../artifacts/contracts/magiNFT.sol/magiNFT.json")
+
+// create contract from ABI
+const contractAddress = "0xf8d3d42a27e16217bC6125f2074033a612630aBB"
+const nftContract = new web3.eth.Contract(contract.abi, contractAddress)
+
