4f662b57da4666facbef0876f9c555234ea554d0
jaolan <olanjorel@gmail.com>
2021-10-09 01:01:47 -0500

    NFT no longer ownable, open mint

--- contracts/magiNFT.sol
+++ contracts/magiNFT.sol
@@ -7,14 +7,13 @@ import "@openzeppelin/contracts/utils/Counters.sol";
 import "@openzeppelin/contracts/access/Ownable.sol";
 import "@openzeppelin/contracts/token/ERC721/extensions/ERC721URIStorage.sol";
 
-contract MyNFT is ERC721URIStorage, Ownable {
+contract MyNFT is ERC721URIStorage {
     using Counters for Counters.Counter;
     Counters.Counter private _tokenIds;
 
     constructor() public ERC721("magiNFT", "NFT") {}
 
     function mintNFT(address recipient, string memory tokenURI)
-        public onlyOwner
         returns (uint256)
     {
         _tokenIds.increment();
